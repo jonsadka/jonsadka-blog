295eeda8a6deed502d186b9e824b9f3a9e9021db
Jon Sadka <jonsadka@gmail.com>
2018-02-19 14:23:06 -0800

    Updates

--- path---tags-ux-ui-8096d33bb2351ce71aa3.js
+++ path---tags-ux-ui-8096d33bb2351ce71aa3.js
@@ -1,2 +1,2 @@
-webpackJsonp([4836336648314],{398:function(t,e){t.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{frontmatter:{title:"How Apple can catch up to Google ratings and Yelp reviews overnight",path:"/how-apple-can-catch-up-to-google-ratings-and-yelp-reviews-overnight"}}}]}},pathContext:{tag:"UX / UI"}}}});
+webpackJsonp([4836336648314],{399:function(t,e){t.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{frontmatter:{title:"How Apple can catch up to Google ratings and Yelp reviews overnight",path:"/how-apple-can-catch-up-to-google-ratings-and-yelp-reviews-overnight"}}}]}},pathContext:{tag:"UX / UI"}}}});
 //# sourceMappingURL=path---tags-ux-ui-8096d33bb2351ce71aa3.js.map
