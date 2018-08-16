d49485fdf6a7910f2b6069848408c230545c7b66
Jon Sadka <jonsadka@uber.com>
2018-08-16 09:36:10 -0700

    Updates

--- path---tags-ux-ui-a32ac5042b2af955d093.js
+++ path---tags-ux-ui-a32ac5042b2af955d093.js
@@ -1,2 +1,2 @@
-webpackJsonp([4836336648314],{403:function(t,e){t.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{frontmatter:{title:"How Apple can catch up to Google ratings and Yelp reviews overnight",path:"/blog/how-apple-can-catch-up-to-google-ratings-and-yelp-reviews-overnight"}}}]}},pathContext:{tag:"UX / UI"}}}});
+webpackJsonp([4836336648314],{411:function(t,e){t.exports={data:{allMarkdownRemark:{totalCount:1,edges:[{node:{frontmatter:{title:"How Apple can catch up to Google ratings and Yelp reviews overnight",path:"/blog/how-apple-can-catch-up-to-google-ratings-and-yelp-reviews-overnight"}}}]}},pathContext:{tag:"UX / UI"}}}});
 //# sourceMappingURL=path---tags-ux-ui-a32ac5042b2af955d093.js.map
